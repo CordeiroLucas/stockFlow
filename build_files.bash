444450a8442add7218b5a561ff51f1039e44e50b
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-11-27 17:11:06 -0300

    Fix: Correct Vercel rewrites syntax (source/destination)

--- build_files.bash
+++ build_files.bash
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-echo "🚀 Iniciando Build..."
-# 2. Usamos 'python3' (o padrão do ambiente) em vez de forçar 'python3.9'
+echo "🚀 Build..."
 python3 -m pip install -r requirements.txt
-echo "🎨 Coletando arquivos estáticos..."
 python3 manage.py collectstatic --noinput --clear
-
-echo "✅ Build Concluído!"
