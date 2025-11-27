eb221982fde1ad1b68c65f46fe4e9c6b137b3eb3
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-11-27 16:43:55 -0300

    Fix: Adicionado run ao pyproject.toml

--- build_files.bash
+++ build_files.bash
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 echo "🚀 Iniciando Build..."
-python3 -m pip install -r requirements.txt
-
 # 2. Usamos 'python3' (o padrão do ambiente) em vez de forçar 'python3.9'
 echo "🎨 Coletando arquivos estáticos..."
 python3 manage.py collectstatic --noinput --clear
