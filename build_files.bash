1ebe6e20bfa421e51a20ab395f9d5b8109633721
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-11-27 14:12:14 -0300

    Chore: build_files added

--- build_files.bash
+++ build_files.bash
@@ -2,11 +2,8 @@
 
 echo "🚀 Iniciando Build..."
 
-# 1. NÃO precisamos instalar o pip aqui. 
-# O Vercel já leu o requirements.txt e instalou tudo automaticamente antes desse script rodar.
-
 # 2. Usamos 'python3' (o padrão do ambiente) em vez de forçar 'python3.9'
 echo "🎨 Coletando arquivos estáticos..."
-uv run manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
 
 echo "✅ Build Concluído!"
