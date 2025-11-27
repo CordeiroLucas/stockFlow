bad91d6e5cc235dc5eca89816019d6b5864c7a35
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-11-27 17:02:04 -0300

    Fix: Teste adicioando ao install command no vercel

--- build_files.bash
+++ build_files.bash
@@ -2,6 +2,8 @@
 
 echo "🚀 Iniciando Build..."
 # 2. Usamos 'python3' (o padrão do ambiente) em vez de forçar 'python3.9'
+python3 -m pip install -r requirements.txt
+
 echo "🎨 Coletando arquivos estáticos..."
 python3 manage.py collectstatic --noinput --clear
 
